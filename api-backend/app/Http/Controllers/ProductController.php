14836b680d182a80495c4a8208f8c0624fc72291
AkelaDumindu <akeladumindu2000@gmail.com>
2025-06-15 13:52:52 +0530

    create product backend

--- api-backend/app/Http/Controllers/ProductController.php
+++ api-backend/app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Container\Attributes\Storage;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -11,7 +13,15 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+
+        $userId = auth()->user()->id;
+
+        $products = Product::where('user_id', $userId)->get();
+
+        return response()->json([
+            "status" => true,
+            "products" => $products
+        ]);
     }
 
     /**
@@ -19,30 +29,75 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $data = $request->validate([
+            "title" => "required"
+        ]);
+
+        $data["user_id"] = auth()->user()->id;
+        if($request->hasFile("banner_image")){
+            $data["banner_image"] = $request->file("banner_image")->store("produts", "public");
+        }
+
+        Product::create($data);
+
+        return response()->json([
+            "status" => true,
+            "message" => "Product created successfully."
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Product $product)
     {
-        //
+
+        return response()->json([
+            "status" => true,
+            "message" => "Product data found.",
+            "product" => $product
+        ]);
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Product $product)
     {
-        //
+
+        $data = $request->validate([
+            "title" => "required"
+        ]);
+
+        if($request->hasFile("banner_image")){
+            if($product->banner_image){
+                Storage::disk("public")->delete($product->banner_image);
+            }
+
+            $data["banner_image"] = $request->file("banner_image")->store("products", "public");
+        }
+
+        $product->update($data);
+
+        return response()->json([
+            "status" => true,
+            "message" => "Product update successfully.",
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response()->json([
+            "status" => true,
+            "message" => "Product deleted successfully.",
+            "product" => $product
+        ]);
     }
 }
--- api-backend/routes/api.php
+++ api-backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,9 +11,11 @@ Route::post("login", [AuthController::class, "login"])->name('login');
 Route::group([
     "middleware"=>["auth:sanctum"]
 ], function(){
-    
+
     Route::get("profile", [AuthController::class, "profile"])->name('profile');
     Route::get("logout", [AuthController::class, "logout"])->name('logout');
+
+    Route::apiResource("products", ProductController::class);
 });
 
 // Route::get('/user', function (Request $request) {
